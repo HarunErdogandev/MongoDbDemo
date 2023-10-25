b30eb5715c89e0f3bfc39df93c895fcc398660ec
Harun Erdoğan <harunerdgann21@gmail.com>
2023-10-25 22:32:11 +0300

    Add project files.

---
+++ MongoDbDemo/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDbDemo
+{
+    public class PersonModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ MongoDbDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace MongoDbDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string ConnectionString = "mongodb://localhost:27017";
+            string databaseName = "simple_db";
+            string collectionName = "people";
+
+            var client = new MongoClient(ConnectionString);
+
+            var db = client.GetDatabase(databaseName);
+            var collection = db.GetCollection<PersonModel>(collectionName);
+
+            var person = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+
+            await collection.InsertOneAsync(person);
+
+            var results = await collection.FindAsync(_ => true);
+
+            foreach (var result in results.ToList())
+            {
+                Console.WriteLine($"{result.Id}: {result.FirstName}  {result.LastName}");
+            }
+
+        }
+    }
+}
